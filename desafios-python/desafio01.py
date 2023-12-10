6a15d6d84f4608791e5d3384cb0b591816621aab
rebecca <rebeccagamer7@gmail.com>
2023-12-10 04:24:25 -0300

    att

--- desafios-python/desafio01.py
+++ desafios-python/desafio01.py
@@ -1,6 +1,5 @@
 print('------ DESAFIO 01 ------')
 
 Nome = input('Qual é o seu nome? ')
+
 print('Olá ' + Nome + ' ! Prazer em te conhecer!')
-
-
--- desafios-python/desafio03.py
+++ desafios-python/desafio03.py
@@ -1,6 +1,8 @@
-print('------ DESAFIO 04 ------')
+print('------ DESAFIO 03 ------')
 
 n1 = int(input('Digite um número = '))
 n2 = int(input('Digite um número = '))
+
 s = n1 + n2 
+
 print('A soma entre os valores {} e {} é igual a {}!'.format(n1, n2, s))
