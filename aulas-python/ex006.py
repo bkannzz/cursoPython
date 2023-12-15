84c5f3a040a4dc54b804e529b2cc45f6659e8c3e
rebecca <rebeccagamer7@gmail.com>
2023-12-14 21:38:59 -0300

    att

---
+++ aulas-python/ex006.py
@@ -0,0 +1,7 @@
+frase = 'Curso em Video Python'
+frase = frase.replace('Python, Android')
+print(frase)
+print('Curso' in frase)
+
+
+
---
+++ desafios-python/desafio21 - analisandor de textos.py
@@ -0,0 +1,12 @@
+print('------ DESAFIO 21 ------')
+
+nome = str(input(' Digite seu nome completo: '))
+
+maiuscula = nome.upper()
+minuscula = nome.lower()
+quantidade = len(nome) - nome.count(' ')
+primeiro = nome.split()
+mostrar = primeiro[0]
+primeiroo = len(mostrar)
+
+print(' Nome em maiusculo: {}\n Nome em minusculo: {}\n Quantas letras tem seu nome: {}\n Quantas letras tem seu primeiro nome:{}'.format(maiuscula, minuscula, quantidade, primeiroo))
