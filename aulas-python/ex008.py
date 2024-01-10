302340ee257c435324142065fe0e5acf4536e402
rebecca <rebeccagamer7@gmail.com>
2024-01-10 03:56:04 -0300

    att

---
+++ aulas-python/ex008.py
@@ -0,0 +1,9 @@
+print('\033[1;31;43mOlá, Mundo!\033[m')
+
+a = 3
+b = 5
+
+print('Os valores são \033[32m{}\033[m e \033[31m{}'.format(a, b))
+
+nome = 'Rebecca'
+print('Olá! Muito prazer em te conhecer, {}{}{}'.format('\033[4;34m', nome, '\033[m'))
