b4b925c0b3cdfafe6d8d10c79ae6d5ce7e89ffb5
rebecca <rebeccagamer7@gmail.com>
2024-01-26 21:02:14 -0300

    tt

--- desafios-python/desafio84 - listas com pares e ímpares.py
+++ desafios-python/desafio84 - listas com pares e ímpares.py
@@ -1,1 +1,22 @@
 print('------ DESAFIO 84 ------')
+
+lista = [[], []]
+valor = 0 
+
+for c in range(1, 8):
+    valor = int(input(f'Digite o {c}o. valor: '))
+
+    if valor % 2 == 0:
+        lista[0].append(valor)
+
+    else:
+        lista[1].append(valor)
+           
+print('=-' * 30)
+
+lista[0].sort()
+lista[1].sort()
+
+print(f'Os valores pares digitados foram: {lista[0]}')
+print(f'Os valores ímpares digitados foram: {lista[1]}')
+
