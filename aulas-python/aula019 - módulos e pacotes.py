b8423920d484388d681bc1e1f6a316a575983b83
bka. <123714003+bkannzz@users.noreply.github.com>
2024-08-23 10:54:42 -0300

    atualizações

---
+++ aulas-python/aula019 - módulos e pacotes.py
@@ -0,0 +1,8 @@
+from aula019 import numero
+
+num = int(input('Digite um valor: '))
+fat = numero.fatorial(num)
+
+print(f'O fatorial de {num} é {fat}.')
+print(f'O dobro de {num} é {numero.dobro(num)}')
+print(f'O triplo de {num} é {numero.triplo(num)}')
