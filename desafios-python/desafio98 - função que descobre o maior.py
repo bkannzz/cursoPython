7b3e8a12c8835bfd79db3fb8b14f22b5db5a6a67
rebecca <rebeccagamer7@gmail.com>
2024-09-21 17:32:53 -0300

    att

---
+++ desafios-python/desafio98 - função que descobre o maior.py
@@ -0,0 +1,26 @@
+print('------ DESAFIO 98 ------')
+
+from time import sleep
+
+def maior(* num):
+    cont = maior = 0
+
+    print('\nAnalisando os valores passados...')
+    
+    for valor in num:
+        print(f'{valor} ', end='', flush=True)
+        sleep(0.3)
+
+        if cont == 0:
+                maior = valor
+        else:
+            if valor > maior:
+                maior = valor
+        cont += 1
+
+    print(f'Foram informados {cont} valores ao todo.')
+    print(f'O maior valor informado foi {maior}')
+
+maior(2, 9, 7, 3, 5, 4)
+maior(7, 4, 8)
+maior(1, 6, 7, 8, 9)
---
+++ desafios-python/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+
+Nome = input('Qual é o seu nome? ')
+
+print('Olá ' + Nome + ' ! Prazer em te conhecer!')
